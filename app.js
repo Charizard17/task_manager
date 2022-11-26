e9af50e227f030ebe2fe579ce7cec633dd17ea29
Esad Dursun <3723esad@gmail.com>
2022-11-26 13:07:33 +0100

    connectdb created. dotenv installed and env variable implemented

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const tasks = require('./routes/tasks');
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+const port = 3000;
 
 // middleware
 app.use(express.json());
 
 // routes
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App');
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
 });
 
-app.use('/api/v1/tasks',tasks);
+app.use("/api/v1/tasks", tasks);
 
-const port = 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(`connectDB error: ${error}`);
+  }
+};
 
-app.listen(port, console.log(`server is listening on port ${port}...`));
+start();
