ffad817b9893a348f00ab2de28caec5618db7a36
Charizard17 <3723esad@gmail.com>
2022-11-27 15:29:18 +0100

    notFound route created. some status option variants added

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
 const port = 3000;
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
 
 const start = async () => {
   try {
